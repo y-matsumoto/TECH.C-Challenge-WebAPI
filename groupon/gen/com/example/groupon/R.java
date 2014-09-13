045017ab2adcba62c2429b9045f77056c97dee8b
TgT <tomolikedoas@gmail.com>
2014-09-13 19:04:54 +0900

    up

--- groupon/gen/com/example/groupon/R.java
+++ groupon/gen/com/example/groupon/R.java
@@ -25,19 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int btn1=0x7f080000;
-        public static final int get_latitude=0x7f080005;
-        public static final int get_longitude=0x7f080007;
-        public static final int latitude=0x7f080004;
-        public static final int longitude=0x7f080006;
-        public static final int test1=0x7f080001;
-        public static final int test2=0x7f080002;
-        public static final int test3=0x7f080003;
+        public static final int action_settings=0x7f080007;
+        public static final int get_latitude=0x7f080004;
+        public static final int get_longitude=0x7f080006;
+        public static final int latitude=0x7f080003;
+        public static final int longitude=0x7f080005;
+        public static final int test1=0x7f080000;
+        public static final int test2=0x7f080001;
+        public static final int test3=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int gpslocation_main=0x7f030001;
+        public static final int listview_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- groupon/src/com/example/groupon/MainActivity.java
+++ groupon/src/com/example/groupon/MainActivity.java
@@ -1,116 +1,55 @@
 package com.example.groupon;
 
-import org.apache.http.HttpEntity;
-
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements LocationListener{
+public class MainActivity extends Activity{
 
-	//LocationManagerを変数定義
-	LocationManager lm;
-	//接続先URL
-	static String url = "http://www.groupon.jp/api-v3/?type=area&format=xml";
-	//緯度
-	static double longitude;
-	//経度
-	static double latitude;
-	//スコープ
-	static int scope = 6;
-	//HttpClient
-	HttpClient httpclient;
+	//GPSLocationを定義
+	GPSLocation glocation = new GPSLocation(this);
+
+	//ReturnCoordinateを定義
+	ReturnValue rvalue = new ReturnValue();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//呼び出されたときに、LocationManagerを取得する。
-		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		//GPSの状態の設定
-		final boolean gpsEnabled = lm.isProviderEnabled(lm.GPS_PROVIDER);
-		//GPSがOFFの時に処理
-		if(!gpsEnabled){
-			//Toastを設定
-			Toast gpsal = Toast.makeText(this, R.string.gpsalert, Toast.LENGTH_LONG);
-			//Toastの表示位置を設定
-			gpsal.setGravity(Gravity.CENTER,0,0);
-			//Toastを表示
-			gpsal.show();
-			//GPS設定画面への遷移設定
-			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-			//GPS設定画面へ遷移
-			startActivity(intent);
-		}
-		//Bundleにパラメータを保存
-		//Bundle bundle = new Bundle();
+	}
 
+	public void onStart(){
+		super.onStart();
+		//GPSが使える状態かチェック
+		glocation.gpsCheck();
 	}
 
 	public void onResume(){
 		super.onResume();
-		if(lm != null){
-			//プロバイダーをGPSに設定する。(100m移動ごとに起爆)
-			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,100,this);
-		}
+
+		//GPSが使える場合プロバイダー等の情報を設定
+		glocation.gpsSet();
+
+		//test用表示処理
+		TextView test1 = (TextView)findViewById(R.id.test1);
+		TextView test2 = (TextView)findViewById(R.id.test2);
+		//TextView test3 = (TextView)findViewById(R.id.test3);
+
+		test1.setText(String.valueOf(rvalue.latitude));
+		test2.setText(String.valueOf(rvalue.longitude));
+		//test3.setText(String.valueOf())
 	}
 
 	public void onPause(){
 		super.onPause();
-		if(lm != null){
-			//位置情報の更新が不要になった時の処理
-			lm.removeUpdates(this);
-		}
 	}
 
-	//座標が変更されたときに呼び出される。
-	@Override
-	public void onLocationChanged(Location location) {
-		// TODO 自動生成されたメソッド・スタブ
-		//緯度と経度を取得
-		latitude  = location.getLatitude();
-		longitude = location.getLongitude();
-
-		//完成版URL
-		String dataurl = url + "&lon=" + longitude + "&lat=" + latitude + "&scope=" + scope;
-
-		httpclient = new HttpClient();
-		HttpEntity entity = httpclient.httpGet(dataurl);
-
-
-		//TextView accessurl = (TextView)findViewById(R.id.test1);
-		//TextView lon = (TextView)findViewById(R.id.test2);
-		//TextView lat = (TextView)findViewById(R.id.test3);
-		//lon.setText(latitude);
-		//lat.setText(longitude);
-		//accessurl.setText(dataurl);
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public void onStop(){
+		super.onStop();
+		//画面が背面に行った場合、GPSを止める
+		glocation.gpsStop();
 	}
 
 	@Override
